72b4f5438187d863072c5a6caa8e956c82303ed9
Rajesh Budhiraja <rajeshbudhiraja5@gmail.com>
2021-08-01 21:50:48 +0530

    Initial Commit

---
+++ Shared/PusherApp.swift
@@ -0,0 +1,17 @@
+//
+//  PusherApp.swift
+//  Shared
+//
+//  Created by Rajesh Budhiraja on 01/08/21.
+//
+
+import SwiftUI
+
+@main
+struct PusherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
